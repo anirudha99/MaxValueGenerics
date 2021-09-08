a6f8d64c43a9f06e3b7254e0ef0343bf2346a2dd
anirudha mayya <anirudha.mayya@gmail.com>
2021-09-08 18:57:57 +0530

    Added use case 2 :Float numbers

--- src/com/maxvalue/MaxMain.java
+++ src/com/maxvalue/MaxMain.java
@@ -3,15 +3,15 @@ package com.maxvalue;
 public class MaxMain {
 	
 	public static void main(String[] args) {
-		Integer a1 = 30,b1 = 10,c1 = 20;
+		Float a1 = 30.5f,b1 = 10.3f,c1 = 20.7f;
 		TestMaximum test1 = new TestMaximum(a1,b1,c1);
 		test1.maximum();
 		
-		Integer a2 = 30,b2 = 50,c2 = 20;
+		Float a2 = 30.7f,b2 = 50.2f,c2 = 20.3f;
 		TestMaximum test2 = new TestMaximum(a2,b2,c2);
 		test2.maximum();
 		
-		Integer a3 = 30,b3 = 10,c3 = 40;
+		Float a3 = 30.8f,b3 = 10.1f,c3 = 40.6f;
 		TestMaximum test3 = new TestMaximum(a3,b3,c3);
 		test3.maximum();
 		
