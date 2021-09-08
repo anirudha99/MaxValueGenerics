0fa892e67d66b909338b40b22c89f52479ec5629
anirudha mayya <anirudha.mayya@gmail.com>
2021-09-08 22:02:23 +0530

    Added use case 4 : to add more than 3 values

--- src/com/maxvalue/MaxMain.java
+++ src/com/maxvalue/MaxMain.java
@@ -7,17 +7,21 @@ public class MaxMain {
 		//String
 		String a1 = "Apple",b1 = "Peach",c1 = "Banana";
 		TestMaximum <String> test1 = new TestMaximum(a1,b1,c1);
-		test1.maximum();
+		System.out.println(test1.findMaximum(a1,b1,c1));
 		
 		//Integer
 		Integer a2 = 30,b2 = 50,c2 = 40;
 		TestMaximum <Integer> test2 = new TestMaximum(a2,b2,c2);
-		test2.maximum();
+		System.out.println(test2.findMaximum(a2,b2,c2));
 		
 		//Float
 		Float a3 = 10.5f,b3 = 12.7f,c3 = 17.7f;
 		TestMaximum <Float> test3 = new TestMaximum(a3,b3,c3);
-		test3.maximum();
+		System.out.println(test3.findMaximum(a3,b3,c3));
+		
+		Integer a4 = 25, b4 = 40, c4 = 65, d4 = 75;
+		TestMaximum <Integer> test4 = new TestMaximum(a4,b4,c4,d4);
+		System.out.println(test4.findMaximum(a4,b4,c4,d4));
 		
 	}
 
--- src/com/maxvalue/TestMaximum.java
+++ src/com/maxvalue/TestMaximum.java
@@ -3,7 +3,7 @@ package com.maxvalue;
 //class with generic
 public class TestMaximum <T extends Comparable<T>> {
 	
-	private T a,b,c;
+	private T a,b,c,d;
 	
 	//constructor with generic
 	TestMaximum(T a, T b, T c){
@@ -12,30 +12,32 @@ public class TestMaximum <T extends Comparable<T>> {
 		this.c= c;
 	}
 	
-	/**
-	 * @method to get maximum
-	 */
-	public void maximum() {
-		T res=TestMaximum.findMaximum(a,b,c);
-		System.out.println("Maximum of "+a+", "+b+" and "+c+" is: " +res);
+	TestMaximum(T a, T b, T c, T d){
+		this.a = a;
+		this.b = b;
+		this.c = c;
+		this.d = d;
 	}
 	
-	/**
-	 * @param <T>
-	 * @param a
-	 * @param b
-	 * @param c
-	 * @return max element
-	 */
-	public static <T extends Comparable<T>> T findMaximum(T a, T b, T c) {
-		T max = a;
-		if(b.compareTo(max) > 0) {
-			max = b;
-		}
-		if(c.compareTo(max) > 0) {
-			max = c;
-		}
-		return max;
+	public static <T extends Comparable<T>> T findMaximum(T ...values) {
+//		T max = a;
+//		if(b.compareTo(max) > 0) {
+//			max = b;
+//		}
+//		if(c.compareTo(max) > 0) {
+//			max = c;
+//		}
+//		return max;
+		
+		T max = values[0];
+	    for (T element : values) {
+	        if (element.compareTo(max) > 0) {
+	            max = element;
+	        }
+	    }
+	    return max;
 	}
+	
+	
 
 }
